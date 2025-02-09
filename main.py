98e66a9ca00ff497b12e709a2a47c99f7ff11f78
Mary Chinonye Chukwu <chukwuchinonye063@gmail.com>
2025-02-09 19:00:22 +0000

    updated

---
+++ main.py
@@ -0,0 +1,11 @@
+# practi.py file imported to main.py 
+import practi as pp
+
+pp.Colorchange("r")
+
+print("Super Subroutine")
+
+print("With my ", end="")
+
+pp.Colorchange("p")
+print("new program ", end="")
